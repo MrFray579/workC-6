9c23c2c843db03dd90db503b4c7c4ed41f8296d7
Fray <kozar20000@mail.ru>
2022-07-13 19:58:48 +0300

    array reversal

---
+++ aTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = {6, 8, 1, 4, 9, 2, 7};
+int n = arr.Length;
+int temp = 0;
+
+for (int i = 0; i < n/2; i++)
+{
+    temp = arr[i];
+    arr[i] = arr[n - i - 1];
+    arr[n - i - 1] = temp;
+    
+}
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
+
