dfac2190a8a6858340ce404beba715911ac3aa5a
Fray <kozar20000@mail.ru>
2022-07-14 18:40:57 +0300

    triangle

---
+++ aTask2/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 20;
+int b = 15;
+int c = 30;
+
+bool checkA = a < c + b;
+bool checkB = b < c + a;
+bool checkC = c < a + b;
+
+if  (checkA && checkB && checkC)
+Console.WriteLine("Треугольник со сторонами a b c существует");
+else
+Console.WriteLine("Треугольник со сторонами a b c не существует");
+
+
+
