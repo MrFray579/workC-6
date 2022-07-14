d686ccfb5e1d07880c65c1e0553ea8ff4ee04486
Fray <kozar20000@mail.ru>
2022-07-14 19:21:26 +0300

    Fibonacci

---
+++ aTask4/Program.cs
@@ -0,0 +1,25 @@
+﻿void Fibonacci(int n)
+{
+    int fib1 = 0;
+    int fib2 = 1;
+    int sum = 0;
+
+    if(n >= 1)
+    Console.WriteLine(fib1);
+
+    if(n >= 2)
+    Console.WriteLine(fib2);
+
+    int i = 0;
+    while (i < n - 2)
+    {
+        sum = fib1 + fib2;
+        Console.WriteLine(sum);
+        fib1 = fib2;
+        fib2 = sum;
+        i++;
+    }
+}
+
+int N = 7;
+Fibonacci(N);
