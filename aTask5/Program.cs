6dfe43d8538fb38df1da3ab6a0356d73ce7a1887
Fray <kozar20000@mail.ru>
2022-07-14 19:43:08 +0300

    Copy

---
+++ aTask5/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = new int [5]{1, 2, 3, 4, 5};
+
+
+int[] copyArray (int [] arr)
+{
+    int len = arr.Length;
+    int[] copy = new int[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        copy[i] = arr[i];
+        Console.Write(copy[i] + ", ");
+    }
+    Console.WriteLine();
+    return copy;
+
+}
+
+int[] result = copyArray(arr);
+
+
